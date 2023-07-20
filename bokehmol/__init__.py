c3024de961af51ffd61a733909047dae7b40e831
Cédric Bouysset <cedric@bouysset.net>
2023-07-20 23:14:16 +0200

    import hover classes at runtime

--- bokehmol/__init__.py
+++ bokehmol/__init__.py
@@ -1,8 +1,9 @@
 from bokehmol import hover
 from bokehmol.config import settings
-from bokehmol.models.rdkit_hover import RDKitHover
-from bokehmol.models.smilesdrawer_hover import SmilesDrawerHover
+from bokehmol.hover import register_alias
 from bokehmol.utils import Hook as _hook
 
-_hook.enable()
+if not settings.dev_mode:
+    _hook.enable()
+
 del _hook
--- bokehmol/models/rdkit_hover.py
+++ bokehmol/models/rdkit_hover.py
@@ -4,6 +4,3 @@ from bokehmol.models.rdkit_formatter import RDKitFormatter
 
 class RDKitHover(BaseHover, RDKitFormatter):
     __implementation__ = "rdkit_hover.ts"
-
-
-RDKitHover.register_alias("rdkit_hover", lambda: RDKitHover())
