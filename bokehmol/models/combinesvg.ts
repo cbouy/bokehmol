d13363e5ff3615f5ed38252440c171045383a44a
Cédric Bouysset <cedric@bouysset.net>
2025-07-15 00:08:01 +0200

    handle rdkit's -1 for width and height

--- bokehmol/models/combinesvg.ts
+++ bokehmol/models/combinesvg.ts
@@ -1,15 +1,48 @@
 export function combineSVGs(
   images: string[], width: number, height: number, maxMolsRow: number
 ): string {
-    let grid = ''
+    let grid: string[] = []
+    let maxWidth = width
+    let maxHeight = height
+    const parser = new DOMParser()
 
     for (let i = 0; i < images.length; i++) {
-      let row = Math.floor(i/maxMolsRow)
-      let col = i % maxMolsRow
-      grid += `<g id="molecule-${i}" transform="translate(${col*width},${row*height})">${images[i]}</g>`
+      let svg = images[i]
+      let imgWidth = width
+      let imgHeight = height
+
+      // handle RDKit's edge case when width or height is set to -1
+      if ((width < 0) || (height < 0)) {
+        // parse directly from SVG element
+        let el = parser.parseFromString(svg, 'image/svg+xml').firstChild
+        if (width < 0) {
+          // @ts-expect-error
+          imgWidth = el.width.baseVal.value
+          if (imgWidth > maxWidth) {
+            maxWidth = imgWidth
+          } else {
+            imgWidth = maxWidth
+          }
+        }
+        if (height < 0)
+          // @ts-expect-error
+          imgHeight = el.height.baseVal.value
+          if (imgHeight > maxHeight) {
+            maxHeight = imgHeight
+          } else {
+            imgHeight = maxHeight
+          }
+      }
+
+      let x = imgWidth * (i % maxMolsRow)
+      let y = imgHeight * Math.floor(i / maxMolsRow)
+      let b64dump = btoa(svg)
+      grid.push(
+        `<image id="molecule-${i}" transform="translate(${x},${y})" href='data:image/svg+xml;base64,${b64dump}'></image>`
+      )
     }
 
-    const parentWidth = width * Math.min(maxMolsRow, images.length)
-    const parentHeight = height * Math.ceil(images.length / maxMolsRow)
-    return `<svg width="${parentWidth}" height="${parentHeight}"><g>${grid}</g></svg>`
+    const parentWidth = maxWidth * Math.min(maxMolsRow, images.length)
+    const parentHeight = maxHeight * Math.ceil(images.length / maxMolsRow)
+    return `<svg width="${parentWidth}" height="${parentHeight}">${grid.join("\n")}</svg>`
 }
