828da1a58b0ad5f6821053ddad041eb79959f5b5
Cédric Bouysset <cedric@bouysset.net>
2025-07-14 19:28:46 +0200

    add __version__ to top module imports

--- bokehmol/__init__.py
+++ bokehmol/__init__.py
@@ -1,5 +1,6 @@
 # ruff: noqa: F401
 from bokehmol import hover
+from bokehmol._version import __version__
 from bokehmol.config import settings
 from bokehmol.hover import register_alias
 from bokehmol.utils import PatchedResources, show
--- bokehmol/_version.py
+++ bokehmol/_version.py
@@ -1,1 +1,2 @@
-__version__ = "0.1.2-a1"
+# X.Y.Z or X.Y.Z-a1
+__version__ = "0.1.2-a3"
